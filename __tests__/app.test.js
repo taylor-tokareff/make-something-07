4173579765e055da9baa3a56748ed00cb938a3e8
taylor-tokareff <taylortokareff@gmail.com>
2021-06-18 12:01:19 -0700

    fixed test import error

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
--- data/setup.js
+++ data/setup.js
@@ -1,18 +1,14 @@
 import { promises as fs } from 'fs';
-import path from 'path';
+
 
 export default (pool) => {
   return fs
     .readFile(
-      path.join(
-        path.dirname(new URL(import.meta.url).pathname),
-        '..',
-        'sql',
-        'setup.sql'
-      ),
+      './sql/setup.sql',
       {
         encoding: 'utf-8',
       }
     )
+
     .then((sql) => pool.query(sql));
 };
