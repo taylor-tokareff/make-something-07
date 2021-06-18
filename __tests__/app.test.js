682ed73882305a52bc0f0df3ffdc0e25dfa1c086
taylor-tokareff <taylortokareff@gmail.com>
2021-06-18 11:00:24 -0700

    first test fails as expected

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,13 @@ describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  test('create an order via Post route', async () => {
+    const res = await request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 420 });
+    expect(res.body).toEqual({ id: '1', quantity: 420 });
+  }
+  );
+
 });
