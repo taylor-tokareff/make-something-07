b8a48c4c11be75f0963928943bcdc97c373d6703
taylor-tokareff <taylortokareff@gmail.com>
2021-06-18 13:02:50 -0700

    fixed test syntax error

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Order from '../lib/models/Order.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
@@ -19,15 +20,15 @@ describe('demo routes', () => {
   );
 
   test('find an order by id via Get route', async () => {
-    const order = await order.insert({
+    const order = await Order.insert({
       quantity: 420
     });
 
-    const res = await request(app.get)(`/api/v1/orders/${order.id}`);
+    const res = await request(app).get(`/api/v1/orders/${order.id}`);
 
     expect(res.body).toEqual(order);
 
   }
-  )
+  );
 
 });
